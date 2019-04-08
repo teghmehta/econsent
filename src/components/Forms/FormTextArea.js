c6710d88ef34ac995a85753ebe8c47966726b0b9
Tegh Mehta <tegh.mehta@gmail.com>
2019-04-08 15:37:22 -0400

    Removed the other text fields, made it dynamic, added it from JSOn data

--- src/components/Forms/FormTextArea.js
+++ src/components/Forms/FormTextArea.js
@@ -4,19 +4,53 @@ import {Col, Row} from "react-bootstrap";
 
 
 class FormTextArea extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            formValue: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({formValue: this.props.formValue });
+    }
+
+    handleChange(text) {
+        this.setState({formValue: text });
+        this.props.changeValue(text, this.props.index);
+    }
+
     render() {
-        return (
-            <Form>
-                <Form.Group as={Row} controlId="exampleForm.ControlTextarea1">
-                    <Form.Label column sm="2">{this.props.formTitle}</Form.Label>
+        if (this.props.numOfRows <= 1) {
+            return (
+                <Form.Group as={Row} controlId="">
+                    <Form.Label column sm="2">
+                        {this.props.formTitle}
+                    </Form.Label>
                     <Col sm="10">
-                        <Form.Control className={'text-area'} as="textarea"
-                                      placeholder={this.props.placeholder}
-                                      rows={this.props.numOfRows} value={this.props.formValue}/>
+                        <Form.Control type="text" placeholder={this.props.placeholder} value={this.state.formValue} onChange={e => this.handleChange(e.target.value)}/>
                     </Col>
                 </Form.Group>
-            </Form>
-        );
+            );
+        } else {
+            return (
+                <Form>
+                    <Form.Group as={Row} controlId="exampleForm.ControlTextarea1">
+                        <Form.Label column sm="2">{this.props.formTitle}</Form.Label>
+                        <Col sm="10">
+                            <Form.Control className={'text-area'} as="textarea"
+                                          placeholder={this.props.placeholder}
+                                          rows={this.props.numOfRows}
+                                          value={this.state.formValue}
+                                          onChange={e => this.handleChange(e.target.value, this.props.index)}
+                            />
+                        </Col>
+                    </Form.Group>
+                </Form>
+            );
+        }
     }
 }
 
--- src/components/Forms/FormsContainer.js
+++ src/components/Forms/FormsContainer.js
@@ -1,37 +1,31 @@
 import React, { Component } from 'react';
 import FormTextArea from "./FormTextArea";
-import FormSingleLine from "./FormSingleLine";
-import FormDoubleLine from "./FormDoubleLine";
 import './Forms.css';
 import {ButtonToolbar, Button} from "react-bootstrap";
+let fs = require("fs");
+let formData = require('../../constants/constants');
 
 class FormsContainer extends Component {
+
+    changeValue(value, index, form) {
+        formData[index].value = value;
+        console.log(formData);
+    }
+
+    save() {
+        let data = JSON.stringify(formData)
+        fs.writeFile('student-3.json', data, (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+        });
+    }
+
     render() {
         return (
             <div className={'forms-container'}>
-                <FormSingleLine formTitle={'Study Title:'} placeholder={'Add Study Title'}/>
-                <FormSingleLine formTitle={'Principal Investigator:'} placeholder={'Add Principal Investigator'}/>
-                <FormSingleLine formTitle={'Funder:'} placeholder={'Add Funder'}/>
-                <FormSingleLine formTitle={'Co-Investigators:'} placeholder={'Add Co-Investigators'}/>
-                <FormTextArea formTitle={'INFORMED CONSENT'} numOfRows={9}/>
-                <FormTextArea formTitle={'INTRODUCTION'} numOfRows={4}/>
-                <FormTextArea formTitle={'WHY IS THIS STUDY BEING DONE?'} numOfRows={4}/>
-                <FormTextArea formTitle={'WHAT IS THE USUAL TREATMENT?'} numOfRows={4}/>
-                <FormTextArea formTitle={'WHAT WILL HAPPEN DURING THIS STUDY?'} numOfRows={4}/>
-                <FormTextArea formTitle={'HOW MANY PEOPLE WILL TAKE PART IN THIS STUDY?'} numOfRows={4}/>
-                <FormTextArea formTitle={'WHAT ARE THE RESPONSIBILITIES OF STUDY PARTICIPANTS?'} numOfRows={4}/>
-                <FormTextArea formTitle={'WHAT ARE THE RISKS OR HARMS OF PARTICIPATING IN THIS STUDY?'} numOfRows={4}/>
-                <FormTextArea formTitle={'WHAT ARE THE RISKS OR HARMS OF PARTICIPATING IN THIS STUDY?'} numOfRows={4}/>
-                <FormTextArea formTitle={'CAN PARTICIPATION IN THIS STUDY END EARLY?'} numOfRows={4}/>
-                <FormTextArea formTitle={'WHAT HAPPENS IF I HAVE A RESEARCH RELATED INJURY?'} numOfRows={4}/>
-                <FormTextArea formTitle={'ARE STUDY PARTICIPANTS PAID TO PARTICIPATE IN THIS STUDY?'} numOfRows={4}/>
-                <FormTextArea formTitle={'HOW WILL MY INFORMATION BE KEPT CONFIDENTIAL?'} numOfRows={4}/>
-                <FormTextArea formTitle={'ARE THERE ANY CONFLICTS OF INTEREST/RELATIONSHIPS?'} numOfRows={4}/>
-                <FormTextArea formTitle={'WHAT ARE THE RIGHTS OF PARTICIPANTS IN A RESEARCH STUDY?'} numOfRows={4}/>
-                <FormTextArea formTitle={'DOCUMENTATION OF INFORMED CONSENT'} numOfRows={18}/>
-                <FormSingleLine formTitle={'Name of Participant:'}/>
+                {formData.map((form, index) => <FormTextArea key={index} index={index} formTitle={form.title} formValue={form.value} placeholder={form.title} numOfRows={form.numOfRows} changeValue={(value, index) => this.changeValue(value, index, form)}/> )}
                 <ButtonToolbar>
-                    <Button variant="primary">Save</Button>
+                    <Button onClick={this.save.bind(this)} variant="primary">Save</Button>
                     <Button variant="secondary">Save and Continue Later</Button>
                     <Button variant="outline-danger">Save and Submit</Button>
                 </ButtonToolbar>
